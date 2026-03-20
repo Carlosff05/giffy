b245838a1affb8594598eac3bd4ffb5335e27d9e
Carlos Fernández <carlos.ffernandez@cojali.com>
2026-03-20 13:59:46 +0100

    servicio login fetch con nulls

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,11 +1,16 @@
 import { useCallback, useContext } from "react";
 import Context from "../context/UserContext";
+import loginService from "../services/login";
 
 export default function useUser() {
     const {jwt, setJwt} = useContext(Context)
 
-    const login = useCallback(() => {
-        setJwt('test')
+    const login = useCallback(({username, password}) => {
+        loginService({username, password})
+        .then(jwt => {setJwt(jwt)})
+        .catch(err => {
+            console.error(err)
+        })
     }, [setJwt]) 
 
     const logout = useCallback(() => {
--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,19 @@
-const ENDPOINT = 'https://localhost:5227'
+const ENDPOINT = 'http://localhost:5227'
 
-export default function login ({username, password}) {
-    return fetch(`${ENDPOINT}/`)
+export default function loginService ({nombre, contrasenna}) {
+    return fetch(`${ENDPOINT}/api/Clientes`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ nombre, contrasenna })
+  })
+    .then(res => {
+      if (!res.ok) throw new Error('Response is NOT ok')
+      return res.json()
+    })
+    .then(res => {
+      const { jwt } = res
+      return jwt
+    })
 }
