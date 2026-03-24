9e5abbc5aa98adb708c833dc57f045bb75d2bfa1
Carlos Fernández <carlos.ffernandez@cojali.com>
2026-03-24 08:29:48 +0100

    Pagina error con emotion

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,42 +1,40 @@
-/** @jsx */
+/** @jsxImportSource @emotion/react */
+import React from "react"
 import { Helmet } from "react-helmet"
 import SearchForm from "../../components/SearchForm"
 import { Link } from "wouter"
 import {css, jsx} from '@emotion/react'
 
 const pageErrorStyles = css`
+    display: flex
     flex-direction: column;
     justify-content: center;
     align-items: center;
     padding: 1rem;
     text-align: center;
 `
-/**
- *
 
-.page-error .code-error {
-  font-size: 5rem;
-  font-weight: bold;
-  font-style: italic;
-}
+const code_error = css`
+    font-size: 5rem;
+    font-weight: bold;
+    font-style: italic;
+`
 
-.page-error .msg-error {
-  font-size: 1.5rem;
-  margin: 1rem 0;
-}
+const msg_error = css`
+    font-size: 1.5rem;
+    margin: 1rem 0;
+`
+const gif_error = css`
+    margin: 1rem auto;
+    width: 250px;
+    height: 250px;
+`
 
-.page-error .gif-error {
-  margin: 1rem auto;
-  width: 250px;
-  height: 250px;
-}
- */
-
-const gifsErrors = ['d2jjuAZzDSVLZ5kI', 'Bp3dFfoqpCKFyXuSzp', 'hv5AEBpH3ZyNoRnABG', 'hLwSzlKN8Fi6I']
+const gifsErrors = ['d2jjuAZzDSVLZ5kI', 'hv5AEBpH3ZyNoRnABG', 'hLwSzlKN8Fi6I']
 
 export default function ErrorPage() {
     const randomImage = () => {
-        return `https://media.giphy.com/media/${gifsErrors[Math.floor(Math.random() * gifsErrors.length) + 1]}/giphy.gif`
+        return `https://media.giphy.com/media/${gifsErrors[Math.floor(Math.random() * gifsErrors.length)]}/giphy.gif`
     }
 
     return (
@@ -49,9 +47,12 @@ export default function ErrorPage() {
             </header>
             <div className="App-wrapper">
                 <div css={pageErrorStyles}>
-                    <span className="code-error">404</span>
-                    <span className="msg-error">Te perdiste, inutil</span>
-                    <img className="gif-error" src={randomImage()} alt="alt-page-404"/>
+                    <span css={code_error}>404</span>
+                    <br/>
+                    <span css={msg_error}>Te perdiste, inutil</span>
+                    <br/>
+                    <img css={gif_error} src={randomImage()} alt="alt-page-404"/>
+                    <br/>
                     <Link to="/" className='btn'>Go to home</Link>
                 </div>
             </div>
